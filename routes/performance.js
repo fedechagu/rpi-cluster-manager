1ade2522507082b4eca2599364f46c7c7d9e71b6
fedechagu <fedechagu@gmail.com>
2016-02-10 20:43:38 -0800

    Add sample performance data

---
+++ routes/performance.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET cpu usage */
+router.get('/cpu', function(req, res, next) {
+  res.send('80%');
+});
+
+/* GET memory usage */
+router.get('/memory', function(req, res, next) {
+  res.send({
+    'available': 12312,
+    'used': 1234
+  });
+});
+
+module.exports = router;
