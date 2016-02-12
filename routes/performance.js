4800076006294a7f3245612c1fe9c20b08695ba4
fedechagu <fedechagu@gmail.com>
2016-02-11 20:38:30 -0800

    Added mosquito image

--- routes/performance.js
+++ routes/performance.js
@@ -1,14 +1,9 @@
 var express = require('express'),
-    router = express.Router(),
-    util  = require('util'),
-    spawn = require('child_process').spawn;
+    router = express.Router();
 
 /* GET cpu usage */
 router.get('/cpu', function(req, res, next) {
-  var memory  = spawn('sh', ['../bash_scripts_available_memory.sh']);
-  ls.stdout.on('data', function (data) {
-    res.send(data);
-  });
+  res.send('80%');
 });
 
 /* GET memory usage */
